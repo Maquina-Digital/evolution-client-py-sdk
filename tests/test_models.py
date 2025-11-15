a889932002625538e1626ef6fcea505006b1e416
jonas <leao.jonas@outlook.com>
2025-11-15 13:14:14 +0100

    fix: remove unused test function from test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -17,6 +17,4 @@ def test_models_poll():
     m = PollMessage(number="324701234567", name="Q", selectableCount=1, values=["A","B"])
     assert m.type == "poll"
 
-def test_models_poll_fail():
-    m = PollMessage(number="324701234567", name="Q", selectableCount=1, values=["A","B"])
-    assert m.type == "poll..."
+
